3ded9a165ccb694dd4a4e63100473fdaf34a53a7
Jonathan Garnica <jony_carp_01@hotmail.com>
2022-08-30 20:20:06 -0300

    .

---
+++ src/Components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Foto from "../Asset/My_Office_Noona’s_Story.jpg"
+
+const NavBar = () => {
+  return (
+
+    <div className='Nav'>
+        <h1>Electro Mundo</h1>
+        <p className='NavTitle'>Inicio</p>
+        <p className='NavTitle'>Categorias</p>
+        <p className='NavTitle'>Ofertas</p>
+        
+    </div>
+
+  )
+}
+
+export default NavBar
